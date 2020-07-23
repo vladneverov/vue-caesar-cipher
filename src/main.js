467d6e57379a43e52c1dc4bb63154421c2b7424c
vladneverov <neverovprod@gmail.com>
2020-07-23 10:59:08 +0300

    init

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
